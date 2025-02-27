1b9412b951ff13f98f41ce4b530cccd52cf4f32a
Todd VanGundy <todd@windsor-hotel.io>
2025-02-27 09:51:34 -0500

    rp-testing

--- bump-version.sh
+++ bump-version.sh
@@ -6,11 +6,14 @@ current_version=$1
 # Split the version into an array
 IFS='.' read -r -a version_parts <<< "$current_version"
 
+echo "Current version: $current_version"
 # Increment the patch version
 version_parts[2]=$((version_parts[2] + 1))
 
 # Join the version parts back into a string
 new_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
+
+echo "New version: $new_version"
 
 # Write the new version back to the VERSION file
 echo "$new_version" > VERSION
