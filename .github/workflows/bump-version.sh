c471c11a72d978e2f9927233f5673bd57c8e279d
Todd VanGundy <todd@windsor-hotel.io>
2025-02-27 09:12:49 -0500

    rp-testing

---
+++ .github/workflows/bump-version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Read the current version from the VERSION file
+current_version=$1
+
+# Split the version into an array
+IFS='.' read -r -a version_parts <<< "$current_version"
+
+# Increment the patch version
+version_parts[2]=$((version_parts[2] + 1))
+
+# Join the version parts back into a string
+new_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
+
+# Write the new version back to the VERSION file
+echo "$new_version" > VERSION
+
+# Output the new version
+echo "Bumped version: $new_version"
